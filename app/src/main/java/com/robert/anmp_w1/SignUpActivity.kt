408215358c5debc72533fe81b598bcbb4ae3bba1
Robert <s160421089@student.ubaya.ac.id>
2024-02-22 15:27:59 +0800

    comment in sign up activity added

--- app/src/main/java/com/robert/anmp_w1/SignUpActivity.kt
+++ app/src/main/java/com/robert/anmp_w1/SignUpActivity.kt
@@ -3,6 +3,7 @@ package com.robert.anmp_w1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/// SIGN UP
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
